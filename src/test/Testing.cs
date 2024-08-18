ad1dc015f81e29861347501cfcf20c9ba10abd8e
yungrixxxi <yungrixxxi@seznam.cz>
2024-08-18 21:52:13 +0200

    Added files for testing the application

---
+++ src/test/Testing.cs
@@ -0,0 +1,21 @@
+﻿using McGuard.src.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McGuard.src.test
+{
+    internal class Testing
+    {
+        public static void Test()
+        {
+
+            ServerManager sm = new ServerManager(256, 512, "server.jar", Environment.CurrentDirectory);
+
+            sm.CreateProcess();
+
+        }
+    }
+}
