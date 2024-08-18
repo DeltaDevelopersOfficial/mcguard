687c70379ac8644fa522bab1e453baaa524a33fe
yungrixxxi <yungrixxxi@seznam.cz>
2024-08-18 21:44:56 +0200

    Added constructor and method CreateProcess

--- src/core/ServerManager.cs
+++ src/core/ServerManager.cs
@@ -1,15 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace McGuard.src.core
 {
     internal class ServerManager
     {
-        public ServerManager()
+        /// <summary>
+        /// Initial memory for server
+        /// </summary>
+        private readonly int initialMemory;
+
+        /// <summary>
+        /// Maximum memory for server
+        /// </summary>
+        private readonly int maximumMemory;
+
+        /// <summary>
+        /// Name of the .jar file of server
+        /// </summary>
+        private readonly string jarName;
+
+        /// <summary>
+        /// Working directory of server
+        /// </summary>
+        private readonly string workingDirectory;
+
+        /// <summary>
+        /// Instance of process
+        /// </summary>
+        private Process serverProcess;
+
+        public ServerManager(int initialMemory, int maximumMemory, string jarName, string workingDirectory)
         {
+            this.initialMemory = initialMemory;
+            this.maximumMemory = maximumMemory;
+            this.jarName = jarName;
+            this.workingDirectory = workingDirectory;
+        }
+
+        /// <summary>
+        /// Create server process
+        /// </summary>
+        public void CreateProcess()
+        {
+            serverProcess = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "java.exe",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = false,
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                }
+            };
+
+            serverProcess.Start();
+            serverProcess.BeginOutputReadLine();
+
+            serverProcess.OutputDataReceived += (object sender, DataReceivedEventArgs e) => Console.WriteLine(e.Data);
         }
     }
 }
