293bf23a2c6bbcd5633b746e216e49d53aeec9b4
yungrixxxi <yungrixxxi@seznam.cz>
2024-08-18 22:06:57 +0200

    Added CommandListener.cs for handling ingame player commands

---
+++ src/listeners/CommandListener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McGuard.src.listeners
+{
+    internal class CommandListener
+    {
+        /// <summary>
+        /// On player chat command receive
+        /// </summary>
+        /// <param name="playerName"></param>
+        /// <param name="issuedCommand"></param>
+        public void OnPlayerCommand(string playerName, string issuedCommand)
+        {
+        }
+    }
+}
