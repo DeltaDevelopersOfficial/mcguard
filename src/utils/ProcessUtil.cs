8a630f32d29388fa43935279c0c2c3150219cdfa
yungrixxxi <yungrixxxi@seznam.cz>
2024-08-18 22:21:06 +0200

    Added method KillProcessByName to ProcessUtil.cs

--- src/utils/ProcessUtil.cs
+++ src/utils/ProcessUtil.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,22 @@ namespace McGuard.src.utils
 {
     internal class ProcessUtil
     {
+        /// <summary>
+        /// Kills process(es) by name
+        /// </summary>
+        /// <param name="name">Process name</param>
+        /// <param name="killAllAssociatedProcesses">Kills all process with specified name if TRUE</param>
+        public static void KillProcessByName(string name, bool killAllAssociatedProcesses)
+        {
+            foreach (var process in Process.GetProcesses())
+            {
+                process.Kill();
 
+                if (!killAllAssociatedProcesses)
+                {
+                    break;
+                }
+            }
+        }
     }
 }
