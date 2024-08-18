3389b1c4b510a61651290eee71ddffd0c9064a17
yungrixxxi <yungrixxxi@seznam.cz>
2024-08-18 22:06:31 +0200

    Added OutputHandler.cs for handling console outputs

---
+++ src/handlers/OutputHandler.cs
@@ -0,0 +1,45 @@
+﻿using McGuard.src.listeners;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McGuard.src.handlers
+{
+    internal class OutputHandler
+    {
+        /// <summary>
+        /// Instance of command listener
+        /// </summary>
+        private CommandListener commandListener;
+
+        private readonly char commandPrefix = '!';
+
+        public OutputHandler()
+        {
+            this.commandListener = new CommandListener();
+        }
+
+        /// <summary>
+        /// On data receive from console output
+        /// </summary>
+        /// <param name="dataReceivedEventArgs"></param>
+        public void OnDataReceive(DataReceivedEventArgs dataReceivedEventArgs)
+        {
+            if (dataReceivedEventArgs != null && dataReceivedEventArgs.Data != null)
+            {
+                string outputData = dataReceivedEventArgs.Data.Trim();
+
+                if (outputData.Contains("<") && outputData.Contains(">") && outputData.Contains("> " + commandPrefix))
+                {
+                    string playerName = outputData.Split(new[] { "> " + commandPrefix }, StringSplitOptions.None)[1].Trim();
+                    string issuedCommand = outputData.Split(new[] { "> " + commandPrefix }, StringSplitOptions.None)[1].Trim();
+
+                    this.commandListener.OnPlayerCommand(playerName, issuedCommand);
+                }
+            }
+        }
+    }
+}
