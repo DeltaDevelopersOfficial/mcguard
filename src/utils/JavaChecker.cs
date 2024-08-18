93efac5ca435fd3b8f9c17edf265a1bb590aa225
yungrixxxi <yungrixxxi@seznam.cz>
2024-08-18 21:45:44 +0200

    Added JavaChecker.cs to check if is java installed on local computer

---
+++ src/utils/JavaChecker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace McGuard.src.utils
+{
+    internal class JavaChecker
+    {
+        /// <summary>
+        /// Checks if is ANY java version installed
+        /// </summary>
+        /// <returns></returns>
+        public static bool CheckJavaInstallation()
+        {
+            try
+            {
+                ProcessStartInfo processInfo = new ProcessStartInfo("java", "-version");
+
+                processInfo.RedirectStandardError = true;
+                processInfo.UseShellExecute = false;
+                processInfo.CreateNoWindow = true;
+
+                using (Process process = Process.Start(processInfo))
+                {
+                    string output = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+
+                    return !string.IsNullOrEmpty(output);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
