aec38c263015b685d7f25e79c503dfb00bd602d6
yungrixxxi <yungrixxxi@seznam.cz>
2024-08-18 21:54:18 +0200

    Added testing code

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using McGuard.src.test;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace McGuard
     {
         static void Main(string[] args)
         {
+            // ONLY FOR TESTING PURPOSES
+            Testing.Test();
         }
     }
 }
