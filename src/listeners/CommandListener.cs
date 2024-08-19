954a4ed2e3eddb0ea9524e4332a90ff7c88ec87d
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-19 14:00:18 +0200

    Added testing command

--- src/listeners/CommandListener.cs
+++ src/listeners/CommandListener.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using McGuard.src.handlers;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace McGuard.src.listeners
 {
-    internal class CommandListener
+    internal class CommandListener : InputHandler
     {
+        public CommandListener(Process process) : base(process)
+        {
+        }
+
         /// <summary>
         /// On player chat command receive
         /// </summary>
@@ -15,6 +21,8 @@ namespace McGuard.src.listeners
         /// <param name="issuedCommand"></param>
         public void OnPlayerCommand(string playerName, string issuedCommand)
         {
+            if (issuedCommand == "test")
+                SendInput("say OK!");
         }
     }
 }
