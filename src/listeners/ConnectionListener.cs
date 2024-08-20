35787d1f791cb4ececb0ba0c084c5c3afce6bca9
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-20 08:57:13 +0200

    Added ConnectionListener.cs

---
+++ src/listeners/ConnectionListener.cs
@@ -0,0 +1,17 @@
+﻿using McGuard.src.handlers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McGuard.src.listeners
+{
+    internal class ConnectionListener : InputHandler
+    {
+        public ConnectionListener(Process process) : base (process)
+        {
+        }
+    }
+}
