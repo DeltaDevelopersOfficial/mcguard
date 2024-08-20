13666a29bb01e1c09ec951c9817f14f3ccaa0c38
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-20 22:31:27 +0200

    Added attributes and constructor

--- src/structures/Message.cs
+++ src/structures/Message.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using McGuard.src.handlers;
+using McGuard.src.structures.text;
 namespace McGuard.src.structures
 {
-    internal class Message
+    internal struct Message
     {
+        /// <summary>
+        /// Content of the message
+        /// </summary>
+        public string Content;
+
+        /// <summary>
+        /// Message length
+        /// </summary>
+        public long Length;
+
+        /// <summary>
+        /// Message color style
+        /// </summary>
+        public Color Color;
+
+        /// <summary>
+        /// Font style like bold, italic etc..
+        /// </summary>
+        public Style Style;
+
+        public Message(string content, long length, Color color, Style style)
+        {
+            this.Content = content;
+            this.Length = length;
+            this.Color = color;
+            this.Style = style;
+        }
     }
 }
