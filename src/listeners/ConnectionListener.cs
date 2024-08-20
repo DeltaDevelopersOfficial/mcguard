b857239953fc75057ab3ff187c62039532f51f0b
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-20 09:52:45 +0200

    Added welcome message

--- src/listeners/ConnectionListener.cs
+++ src/listeners/ConnectionListener.cs
@@ -1,4 +1,6 @@
 ﻿using McGuard.src.handlers;
+using McGuard.src.structures;
+using McGuard.src.utils;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,8 +12,19 @@ namespace McGuard.src.listeners
 {
     internal class ConnectionListener : InputHandler
     {
+        private readonly string[] joinMessage;
+
         public ConnectionListener(Process process) : base (process)
         {
+            this.joinMessage = ConfigManager.GetValueByKey("joinmsg").Split('§');
+        }
+
+        public void OnPlayerConnection(Player player)
+        {
+            foreach (var singleMessage in joinMessage)
+            {
+                SendMessageToPlayer(player, singleMessage);
+            }
         }
     }
 }
