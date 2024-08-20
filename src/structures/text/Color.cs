6ec929e77325dc32161c3c88a75154d571ba6c54
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-20 22:26:46 +0200

    Added minecraft message font style like color and style

---
+++ src/structures/text/Color.cs
@@ -0,0 +1,22 @@
+﻿namespace McGuard.src.structures.text
+{
+    internal enum MinecraftColors
+    {
+        Black,
+        DarkBlue,
+        DarkGreen,
+        DarkAqua,
+        DarkRed,
+        DarkPurple,
+        Gold,
+        Gray,
+        DarkGray,
+        Blue,
+        Green,
+        Aqua,
+        Red,
+        LightPurple,
+        Yellow,
+        White
+    }
+}
---
+++ src/structures/text/Style.cs
@@ -0,0 +1,12 @@
+﻿namespace McGuard.src.structures.text
+{
+    internal enum FontStyle
+    {
+        Bold,
+        Italic,
+        Underlined,
+        Strikethrough,
+        Obfuscated,
+        Default
+    }
+}
