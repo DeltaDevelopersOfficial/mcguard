6e493eacd127d631752a65dcfef7680ee5e67560
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-21 14:18:08 +0200

    Added attributes and constructor

--- src/structures/Command.cs
+++ src/structures/Command.cs
@@ -8,5 +8,20 @@ namespace McGuard.src.structures
 {
     internal struct Command
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string Name;
+           
+        /// <summary>
+        /// Command arguments
+        /// </summary>
+        public string[] Arguments;
+        
+        public Command(string name, string[] arguments)
+        {
+            Name = name;
+            Arguments = arguments;
+        }
     }
 }
