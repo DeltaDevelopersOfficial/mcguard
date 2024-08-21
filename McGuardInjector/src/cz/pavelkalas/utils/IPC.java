21fe512ccce683651b1ba24e78c1e5695eff3747
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-21 12:22:05 +0200

    Added constructor with named pipe sender

--- McGuardInjector/src/cz/pavelkalas/utils/IPC.java
+++ McGuardInjector/src/cz/pavelkalas/utils/IPC.java
@@ -1,4 +1,31 @@
 package cz.pavelkalas.utils;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class IPC {
+
+	/*
+	 * Process ID as named pipe path
+	 */
+	private final int processId;
+	
+	public IPC(int processId) {
+		this.processId = processId;
+	}
+	
+	/*
+	 * Sends a data string to pipe
+	 */
+	public void sendToPipe(String pipeMessage) {
+		String pipeName = "\\\\.\\pipe\\" + processId;
+        
+		try (OutputStream outputStream = new FileOutputStream(pipeName)) {
+            outputStream.write(pipeMessage.getBytes());
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
 }
