b2fc4e024fff49dc3213fd31da13b397f133f501
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-22 10:35:59 +0200

    Added first test string

--- src/content/StringManager.cs
+++ src/content/StringManager.cs
@@ -8,5 +8,35 @@ namespace McGuard.src.content
 {
     internal class StringManager
     {
+        private static List<string> stringsList = new List<string>();
+
+        public static void Setup()
+        {
+            AddString("Insufficient administrator privileges");
+        }
+
+        /// <summary>
+        /// Add string to list
+        /// </summary>
+        /// <param name="str"></param>
+        public static void AddString(string str)
+        {
+            stringsList.Add(str);
+        }
+
+        /// <summary>
+        /// Gets string from list
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static string GetString(int index)
+        {
+            if (index < stringsList.Count && index >= 0)
+            {
+                return stringsList[index];
+            }
+
+            return null;
+        }
     }
 }
