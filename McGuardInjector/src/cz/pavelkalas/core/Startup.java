94e7d8c0e217efcb717a243c0abdfee57f4325f7
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-22 11:01:38 +0200

    Registered ConnectionListener class as listener

--- McGuardInjector/src/cz/pavelkalas/core/Startup.java
+++ McGuardInjector/src/cz/pavelkalas/core/Startup.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import cz.pavelkalas.listeners.ChatListener;
+import cz.pavelkalas.listeners.ConnectionListener;
 import cz.pavelkalas.utils.IPC;
 
 public class Startup extends JavaPlugin {
@@ -11,7 +12,9 @@ public class Startup extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		IPC ipc = new IPC("" + Bukkit.getPort());
+
 		this.getServer().getPluginManager().registerEvents(new ChatListener(ipc), this);
+		this.getServer().getPluginManager().registerEvents(new ConnectionListener(ipc), this);
 	}
 
 	@Override
