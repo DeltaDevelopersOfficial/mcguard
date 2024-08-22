09755f12af0f442b6c87a63143bef9e14f7ceb2f
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-22 11:09:10 +0200

    Added logic for add, remove and find player in list

--- src/core/PlayerManager.cs
+++ src/core/PlayerManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using McGuard.src.structures;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,42 @@ namespace McGuard.src.core
 {
     internal class PlayerManager
     {
+        /// <summary>
+        /// List of players
+        /// </summary>
+        private List<Player> playerList = new List<Player>();
+
+        /// <summary>
+        /// Adds player to list
+        /// </summary>
+        /// <param name="player">Player instance</param>
+        public void AddPlayer(Player player)
+        {
+            if (!playerList.Contains(player))
+            {
+                playerList.Add(player);
+            }
+        }
+
+        /// <summary>
+        /// Removes player from list
+        /// </summary>
+        /// <param name="player"></param>
+        public void RemovePlayer(Player player)
+        {
+            if (playerList.Contains(player))
+            {
+                playerList.Remove(player);
+            }
+        }
+
+        /// <summary>
+        /// Tries to find a player in list
+        /// </summary>
+        /// <param name="playerName">Player instance</param>
+        public Player FindPlayer(string playerName)
+        {
+            return playerList.Where(p => p.Name == playerName).Select(p => p).First();
+        }
     }
 }
