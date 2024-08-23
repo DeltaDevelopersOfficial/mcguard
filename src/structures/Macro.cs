ad9cc0fcd62ae094e7c6279058555405b7c11518
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-23 13:10:31 +0200

    Added attribute commandList for macro

--- src/structures/Macro.cs
+++ src/structures/Macro.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using McGuard.src.handlers;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,14 @@ namespace McGuard.src.structures
 {
     internal class Macro
     {
+        /// <summary>
+        /// List of commands
+        /// </summary>
+        public List<string> commandList = new List<string>();
+
+        public Macro(List<string> commandList)
+        {
+            this.commandList = commandList;
+        }
     }
 }
