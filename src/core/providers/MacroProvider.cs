e8ff38bc1efae9591e1d3c89e08009b3a0e64a2f
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-23 12:22:53 +0200

    Added Macro provider

---
+++ src/core/providers/MacroProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McGuard.src.core.providers
+{
+    internal class MacroProvider
+    {
+    }
+}
