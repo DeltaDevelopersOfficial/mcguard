99005cc3d634771c2f95acbe82b002e454bb4e1b
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-25 15:50:49 +0200

    Added GetSha1FromText to convert plain text to SHA-1

--- src/utils/CryptographyStrategy.cs
+++ src/utils/CryptographyStrategy.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace McGuard.src.utils
 {
     internal class CryptographyStrategy
     {
+        /// <summary>
+        /// Converts text to SHA-1
+        /// </summary>
+        /// <param name="input">String your want convert to SHA-1</param>
+        /// <returns>SHA1 hash</returns>
+        public static string GetSha1FromText(string input)
+        {
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                byte[] hashBytes = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                StringBuilder hashString = new StringBuilder();
+                
+                foreach (byte b in hashBytes)
+                {
+                    hashString.Append(b.ToString("x2"));
+                }
+
+                return hashString.ToString();
+            }
+        }
     }
 }
