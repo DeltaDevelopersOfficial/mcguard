a5d1079825d8ab65f0c876f4591b96d4a0fd4068
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-26 13:34:52 +0200

    Added command result enum

---
+++ src/structures/enums/CommandResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace McGuard.src.structures.enums
+{
+    /// <summary>
+    /// Command result
+    /// </summary>
+    [Flags]
+    public enum CommandResult
+    {
+        Success = 0x1,                  // successfully command handled
+        Failed = 0x2,                   // command not handled (probably because an unknown command)
+        NotAvailableFromConsole = 0x4,  // handled game command from console
+    }
+}
