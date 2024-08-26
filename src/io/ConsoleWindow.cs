8031412e4b3f62922204ab722a9fa768895190a3
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-26 13:50:36 +0200

    Added print method with custom prefix, like time, or log type, etc..

--- src/io/ConsoleWindow.cs
+++ src/io/ConsoleWindow.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace McGuard.src.io
 {
     internal class ConsoleWindow
     {
+        /// <summary>
+        /// Writes data to console with prefix if specified
+        /// </summary>
+        /// <param name="message">Text to print</param>
+        /// <param name="prefix">Prefix is text before text</param>
+        public static void WriteLine(string message, string prefix = null)
+        {
+            if (prefix == null)
+            {
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine(prefix + " " + message);
+            }
+        }
     }
 }
