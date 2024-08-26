fe402e5ebba1d85eba287bb10a9f8a894e411e17
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-26 14:36:58 +0200

    Added function for log new lines into log file

--- src/io/FileManager.cs
+++ src/io/FileManager.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace McGuard.src.io
 {
-    internal class FileLogger
+    internal class FileManager
     {
+        /// <summary>
+        /// Log line into specified log file
+        /// </summary>
+        /// <param name="fileName">Path to log file</param>
+        /// <param name="newLine">New line to log</param>
+        public static void LogFileLine(string fileName, string newLine)
+        {
+            newLine = newLine.Trim();
+
+            if (!File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, newLine + "\n");
+            }
+            else
+            {
+                string content = File.ReadAllText(fileName);
+                content += newLine + "\n";
+                File.WriteAllText(fileName, content);
+            }
+        }
     }
 }
