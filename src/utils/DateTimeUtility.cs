265c0844d44abeea690947be9f9b9de61e2ec881
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-26 13:54:39 +0200

    Added function to get only time

--- src/utils/DateTimeUtility.cs
+++ src/utils/DateTimeUtility.cs
@@ -8,5 +8,20 @@ namespace McGuard.src.utils
 {
     internal class DateTimeUtility
     {
+        /// <summary>
+        /// Get only time in format xx:xx:xx (format 12-24hrs is by locale settings)
+        /// </summary>
+        /// <returns>Only time string in format xx:xx:xx</returns>
+        public static string GetCurrentTime()
+        {
+            string time = DateTime.Now.TimeOfDay.ToString();
+
+            if (time.Contains("."))
+            {
+                time = time.Split('.')[0];
+            }
+
+            return time;
+        }
     }
 }
