ef1345974c4ada465cdc44b3fa16d094aee46aef
Pavel Kalaš <yungrixxxi@seznam.cz>
2024-08-27 18:38:12 +0200

    Added process afinity function to limit CPU's to server

--- src/utils/ProcessUtil.cs
+++ src/utils/ProcessUtil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,6 +24,30 @@ namespace McGuard.src.utils
                 if (!killAllAssociatedProcesses)
                 {
                     break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets the processor affinity for a process by its name
+        /// </summary>
+        /// <param name="coreCount">Number of cores the process can use. Use 0 (or less) for all available cores</param>
+        /// <param name="processName">The name of the process.</param>
+        public static void SetProcessAffinity(int coreCount, Process serverProcess)
+        {
+            if (serverProcess != null && !serverProcess.HasExited)
+            {
+                if (coreCount <= 0)
+                {
+                    // Use all available cores
+                    serverProcess.ProcessorAffinity = (IntPtr)(-1);
+                }
+                else
+                {
+                    // Generate a bitmask to use the specified number of cores
+                    // Example: coreCount = 2 => 0b00000011 (binary) = 0x3 (hexadecimal)
+                    IntPtr mask = (IntPtr)((1 << coreCount) - 1);
+                    serverProcess.ProcessorAffinity = mask;
                 }
             }
         }
